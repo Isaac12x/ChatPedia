167ccbe6f16934ec345f45eae2835c9d3e689701
Christine Le <christinele.cs@gmail.com>
2016-02-27 14:53:20 -0800

    added and hooked up more fields from user and chat room data models

--- ChatPedia/ChatViewController.swift
+++ ChatPedia/ChatViewController.swift
@@ -16,12 +16,15 @@ class ChatViewController: JSQMessagesViewController {
     var outgoingBubbleImageView: JSQMessagesBubbleImage!
     var incomingBubbleImageView: JSQMessagesBubbleImage!
     
-    var roomRef: Firebase!
-    var messageRef: Firebase!
+    var currentRoomId = "-KBWKykTqc5R3NT4C1qt"  // set Hop-On Hop-Off Bus Tour as default
+    
+    // Firebase references.
+    var messageRef     : Firebase!
+    var roomRef        : Firebase!
     
     //typing
     var usersTypingQuery: FQuery!
-
+    
     var userIsTypingRef: Firebase! // 1
     private var localTyping = false // 2
     var isTyping: Bool {
@@ -34,19 +37,37 @@ class ChatViewController: JSQMessagesViewController {
             userIsTypingRef.setValue(newValue)
         }
     }
-
+    
     // MARK: - One Time Load
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
         // Firebase
+        roomRef = CoreFirebaseData.sharedInstance.ref.childByAppendingPath("room")
         messageRef = CoreFirebaseData.sharedInstance.ref.childByAppendingPath("messages")
+        
+        // Get all rooms
+        // TODO: Hook this up later fully.  just print out for now
+        roomRef.observeSingleEventOfType(.Value, withBlock: { snapshot in
+            for room in snapshot.children.allObjects as! [FDataSnapshot] {
+                //print(room.value)
+                self.currentRoomId = room.key
+            }
+        })
+        
+        // Get all chats for the selected room
+        let testRef = messageRef.queryOrderedByChild("roomId").queryEqualToValue("-KBWR2MTrTpJdR1r5Agu")
+        testRef.observeSingleEventOfType(.Value, withBlock: { snapshot in
+            for msg in snapshot.children.allObjects as! [FDataSnapshot] {
+                print(msg.value)
+            }
+        })
+        
         self.senderId = CurrentUser.sharedInstance.authData.uid
-        self.senderDisplayName = CurrentUser.sharedInstance.displayName
+        //self.senderDisplayName = CurrentUser.sharedInstance.displayName
+        self.senderDisplayName = "Jane"
         
         observeTyping()
-
         
         // JSQ
         self.title = "ChatPedia"
@@ -55,7 +76,7 @@ class ChatViewController: JSQMessagesViewController {
         // no avatars
         collectionView!.collectionViewLayout.incomingAvatarViewSize = CGSizeZero
         collectionView!.collectionViewLayout.outgoingAvatarViewSize = CGSizeZero
-
+        
     }
     
     // MARK: - Repeating Load
@@ -73,9 +94,10 @@ class ChatViewController: JSQMessagesViewController {
             // 3
             let id = snapshot.value["senderId"] as! String
             let text = snapshot.value["text"] as! String
+            let senderName = snapshot.value["senderName"] as! String
             
             // 4
-            self.addMessage(id, text: text)
+            self.addMessage(id, text: text, senderName: senderName)
             
             // 5
             self.finishReceivingMessage()
@@ -83,71 +105,89 @@ class ChatViewController: JSQMessagesViewController {
     }
     
     // MARK: - Receiving Messages
-    func addMessage(id: String, text: String) {
-        let message = JSQMessage(senderId: id, displayName: "", text: text)
+    func addMessage(id: String, text: String, senderName: String) {
+        let message = JSQMessage(senderId: id, displayName: senderName, text: text)
         messages.append(message)
     }
-
+    
     // MARK: - Sending Messages
     override func didPressSendButton(button: UIButton!, withMessageText text: String!, senderId: String!,
-        senderDisplayName: String!, date: NSDate!) {
-            
-            let itemRef = messageRef.childByAutoId() // 1
-            let messageItem = [ // 2
-                "text": text,
-                "senderId": senderId
-            ]
-            itemRef.setValue(messageItem) // 3
-            
-            // 4
-            JSQSystemSoundPlayer.jsq_playMessageSentSound()
-            
-            // 5
-            finishSendingMessage()
-            isTyping = false
-
+                                     senderDisplayName: String!, date: NSDate!) {
+        
+        let itemRef = messageRef.childByAutoId() // 1
+        let messageItem = [ // 2
+            "text": text,
+            "senderId": senderId,
+            "roomId": currentRoomId,
+            "senderName": senderDisplayName
+        ]
+        itemRef.setValue(messageItem) // 3
+        
+        // 4
+        JSQSystemSoundPlayer.jsq_playMessageSentSound()
+        
+        // 5
+        finishSendingMessage()
+        isTyping = false
+        
     }
     
     // MARK: - JSQ Collection View
     override func collectionView(collectionView: UICollectionView,
-        cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
-            let cell = super.collectionView(collectionView, cellForItemAtIndexPath: indexPath)
-                as! JSQMessagesCollectionViewCell
-            
-            let message = messages[indexPath.item]
-            
-            if message.senderId == senderId {
-                cell.textView!.textColor = UIColor.whiteColor()
-            } else {
-                cell.textView!.textColor = UIColor.blackColor()
-            }
-            
-            return cell
+                                 cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = super.collectionView(collectionView, cellForItemAtIndexPath: indexPath)
+            as! JSQMessagesCollectionViewCell
+        
+        let message = messages[indexPath.item]
+        
+        if message.senderId == senderId {
+            cell.textView!.textColor = UIColor.whiteColor()
+        } else {
+            cell.textView!.textColor = UIColor.blackColor()
+        }
+        
+        return cell
+    }
+    
+    override func collectionView(collectionView: JSQMessagesCollectionView!, attributedTextForMessageBubbleTopLabelAtIndexPath indexPath: NSIndexPath!) -> NSAttributedString! {
+        let data = self.collectionView(self.collectionView, messageDataForItemAtIndexPath: indexPath)
+        if (self.senderDisplayName == data.senderDisplayName()) {
+            return nil
+        }
+        return NSAttributedString(string: data.senderDisplayName())
+    }
+    
+    override func collectionView(collectionView: JSQMessagesCollectionView!, layout collectionViewLayout: JSQMessagesCollectionViewFlowLayout!, heightForMessageBubbleTopLabelAtIndexPath indexPath: NSIndexPath!) -> CGFloat {
+        let data = self.collectionView(self.collectionView, messageDataForItemAtIndexPath: indexPath)
+        if (self.senderDisplayName == data.senderDisplayName()) {
+            return 0.0
+        }
+        return kJSQMessagesCollectionViewCellLabelHeightDefault
     }
     
     override func collectionView(collectionView: JSQMessagesCollectionView!,
-        messageDataForItemAtIndexPath indexPath: NSIndexPath!) -> JSQMessageData! {
-            return messages[indexPath.item]
+                                 messageDataForItemAtIndexPath indexPath: NSIndexPath!) -> JSQMessageData! {
+        return messages[indexPath.item]
     }
     
     override func collectionView(collectionView: UICollectionView,
-        numberOfItemsInSection section: Int) -> Int {
-            return messages.count
+                                 numberOfItemsInSection section: Int) -> Int {
+        return messages.count
     }
     
     override func collectionView(collectionView: JSQMessagesCollectionView!,
-        messageBubbleImageDataForItemAtIndexPath indexPath: NSIndexPath!) -> JSQMessageBubbleImageDataSource! {
-            let message = messages[indexPath.item] // 1
-            if message.senderId == senderId { // 2
-                return outgoingBubbleImageView
-            } else { // 3
-                return incomingBubbleImageView
-            }
+                                 messageBubbleImageDataForItemAtIndexPath indexPath: NSIndexPath!) -> JSQMessageBubbleImageDataSource! {
+        let message = messages[indexPath.item] // 1
+        if message.senderId == senderId { // 2
+            return outgoingBubbleImageView
+        } else { // 3
+            return incomingBubbleImageView
+        }
     }
     
     override func collectionView(collectionView: JSQMessagesCollectionView!,
-        avatarImageDataForItemAtIndexPath indexPath: NSIndexPath!) -> JSQMessageAvatarImageDataSource! {
-            return nil
+                                 avatarImageDataForItemAtIndexPath indexPath: NSIndexPath!) -> JSQMessageAvatarImageDataSource! {
+        return nil
     }
     
     // MARK: - JSQ Typing Indicator
@@ -188,7 +228,7 @@ class ChatViewController: JSQMessagesViewController {
         incomingBubbleImageView = factory.incomingMessagesBubbleImageWithColor(
             UIColor.jsq_messageBubbleLightGrayColor())
     }
-
-
+    
+    
 }
 
